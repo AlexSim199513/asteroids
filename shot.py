cfb44ea1b46a099bd37197099f21aa6d411f38f4
AlexSim199513 <alex199513@gmail.com>
2025-01-22 16:40:13 -0500

    removed self.radius from shot class

--- shot.py
+++ shot.py
@@ -5,7 +5,6 @@ from constants import SHOT_RADIUS
 class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
-        self.radius = radius
         self.velocity = pygame.Vector2(0,0)
         self.add(self.containers)
 
