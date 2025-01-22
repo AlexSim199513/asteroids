202dd40f2592db461d558c6c07dc5e83ff468335
AlexSim199513 <alex199513@gmail.com>
2025-01-22 16:41:20 -0500

    added containter attribute to shot class

--- shot.py
+++ shot.py
@@ -3,6 +3,9 @@ import pygame
 from constants import SHOT_RADIUS
 
 class Shot(CircleShape):
+
+    containers = None
+    
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
         self.velocity = pygame.Vector2(0,0)
