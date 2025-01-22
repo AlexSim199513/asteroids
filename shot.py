5d72c467ffa516397ae7c41c34f4d44885b6f973
AlexSim199513 <alex199513@gmail.com>
2025-01-22 16:44:48 -0500

    removed self.containers line from shot.py

--- shot.py
+++ shot.py
@@ -5,11 +5,10 @@ from constants import SHOT_RADIUS
 class Shot(CircleShape):
 
     containers = None
-    
+
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
         self.velocity = pygame.Vector2(0,0)
-        self.add(self.containers)
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius)
